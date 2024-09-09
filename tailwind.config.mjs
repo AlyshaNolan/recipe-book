b4f359973c897a27a070248a4431c7cd2f514f22
Alysha Nolan <alysha@cloudcannon.com>
2024-09-09 21:45:16 +1200

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			screens: {
+				'desktop': '768px',
+			},
+			gridTemplateColumns: {
+				// Simple 16 column grid
+				'mobile': '16px 1fr 16px',
+				'desktop': '1fr repeat(12, calc(min(96px, 8%))) 1fr'
+			},
+		}
+	},
+	plugins: [],
+}
